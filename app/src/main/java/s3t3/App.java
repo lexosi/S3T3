2ac421f3c8d7b422a55f3000a706b0e5d3764c7a
m-maso <146754649+m-maso@users.noreply.github.com>
2023-12-21 12:11:33 +0100

    restored files

--- app/src/main/java/s3t3/App.java
+++ app/src/main/java/s3t3/App.java
@@ -3,12 +3,94 @@
  */
 package s3t3;
 
-public class App {
-    public String getGreeting() {
-        return "Hello!";
-    }
+import s3t3.GardenShop.application.port.out.GardenShopRepository;
+import s3t3.GardenShop.application.service.CreateGardenShopService;
+import s3t3.GardenShop.application.utilities.Input_sc;
+import s3t3.GardenShop.domain.GardenShop;
+import s3t3.GardenShop.domain.enums.ProductType;
+import s3t3.GardenShop.infrastructure.adapter.repository.TxtGardenShopRepository;
+import s3t3.GardenShop.infrastructure.adapter.repository.TxtTicketRepository;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public class App {
+	GardenShop gardenShop;
+	CreateGardenShopService service;
+	GardenShopRepository repository;
+//	TxtGardenShopRepository txtGardenRepo;
+//	TxtTicketRepository txtTicketRepo;
+	
+	public App() {
+		//gardenShop = new GardenShop(name)
+		service = new CreateGardenShopService(repository);
+//		txtGardenRepo = new TxtGardenShopRepository();
+//		txtTicketRepo = new TxtTicketRepository();
+	}
+	
+	//menu
+	public void start() {
+		menuStart();
+	}
+	
+	public void menuStart() {
+		boolean quit = false;
+		do {
+			switch(showMenu()) {
+			case 0 -> quit = true;
+			case 1 -> service.createGardenShop(name);
+			case 2 -> addProductToStock();
+//			case 2 -> TxtGardenShopRepository.addProduct(prodType);
+			case 3 -> ;
+			case 4 -> TxtGardenShopRepository.removeProduct(prodType);
+			case 5 -> ;
+			case 6 -> ;
+			case 7 -> ;
+			case 8 -> ;
+			case 9 -> ;
+			default -> System.out.println("Error! Please enter a number between 0 and 9")
+			}
+		} while(!quit);
+	}
+	
+	public static int showMenu() {
+		int option = Input_sc.enterInt("GARDEN SHOP APP MENU\n"
+				+ "Choose one of the following options (enter its number)\n"
+				+ "1. Create new Garden Shop\n"
+				+ "2. Add Tree, Flower or Decoration to the shop's stock\n"
+				+ "3. Show the stock of all products\n"
+				+ "4. Remove Tree, Flower or Decoration from the shop's stock\n"
+				+ "5. Show stock quantities\n"
+				+ "6. Show total value of the Garden shop\n"
+				+ "7. Create sale's tickets with multiple objects\n"
+				+ "8. Show old sale tickets\n"
+				+ "9. Show total amount made from sales\n"
+				+ "0. Quit the app");
+		return option;
+	}
+	
+	public void addProductToStock() {
+		ProductType prodType;
+		Product product;
+		
+		int productType = Input_sc.enterInt("Which type of product do you want to add to the shop:\n"
+				+ "1. Tree\n"
+				+ "2. Flower\n"
+				+ "3. Decoration");
+		double price = Input_sc.enterDouble("Enter the price of the product:");
+		String name = Input_sc.enterStr("Enter the name of the product:");
+		
+		switch(productType) {
+		case 1:
+			prodType = ProductType.TREE;
+			double height = Input_sc.enterDouble("Enter tree's height:");
+			gardenShop.getGardenStock().add(new Product(prodType, price, name));
+//			gardenShop.setGardenStock(product);
+			break;
+		case 2:
+			prodType = ProductType.FLOWER;
+			break;
+		case 3:
+			prodType = ProductType.DECORATION;
+		}
+		
+		TxtGardenShopRepository.addProduct(prodType);
+	}
 }
---
+++ app/src/main/java/s3t3/GardenShop/application/service/AddProductService.java
@@ -0,0 +1,56 @@
+package s3t3.GardenShop.application.service;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import s3t3.GardenShop.application.port.in.AddProduct;
+import s3t3.GardenShop.domain.GardenShop;
+import s3t3.GardenShop.domain.Product;
+import s3t3.GardenShop.domain.enums.ProductType;
+import s3t3.GardenShop.infrastructure.adapter.repository.TxtGardenShopRepository;
+
+public class AddProductService implements AddProduct {
+
+	private final TxtGardenShopRepository repository;
+	private String filePath = "GardenShop.txt";
+
+	public AddProductService(TxtGardenShopRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	public void addProduct(Product product) {
+		GardenShop.
+		}
+		/*
+	@Override
+	public void addProduct(ProductType prodType) {
+		String prod = prodType.toString();
+		
+		try (FileWriter writer = new FileWriter(filePath, true)){
+			writer.write(prod.toString() + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String variable;
+		switch(prodType) {
+		case T:
+			variable = "Tree";
+			break;
+		case F:
+			break;
+		case D:
+			break;
+		}
+		try(FileWriter writer = new FileWriter(filePath, true)) {
+			writer.write(variable.toString() + "\n");
+		}
+		*/
+		
+		repository.save(product);
+	}
+	
+	
+}
